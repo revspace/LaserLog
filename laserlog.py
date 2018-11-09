0f68d1b370cf0ad29f56d2d987638d00978dcf19
Peetz0r <peter@haas-en-berg.nl>
2018-11-09 05:46:27 +0100

    refactor log and run into functions record end times, and reshow UI afterwards

--- laserlog.py
+++ laserlog.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import requests, re, gi, json, os, sys, datetime
+import requests, re, gi, json, os, sys, datetime, subprocess, time
 
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gio
@@ -15,6 +15,27 @@ TEXT_NOT_ON_LIST_TEXT = '''Als je nog geen persoonlijke instructie gehad hebt,
 is het niet de bedoeling dat je de lasercutter gebruikt.
 
 Vraag iemand die wel op de lijst staat of hij/zij jou instructie wilt geven.'''
+
+def run(prog):
+    global win
+    win.close()
+
+    print('Starting %s' % (prog))
+    log('%s,%s,%s' % (name, prog, 'start'))
+
+    p = subprocess.Popen(['./start-%s' % (prog)])
+    while p.poll() is None:
+        time.sleep(0.1)
+        while Gtk.events_pending():
+            Gtk.main_iteration()
+
+    print('%s done' % (prog))
+    log('%s,%s,%s' % (name, 'LaserWeb', 'done'))
+
+    win = LaserLogWindow()
+    win.connect("destroy", Gtk.main_quit)
+    win.show_all()
+    Gtk.main()
 
 def check_path(path):
     t = os.path.realpath(os.path.expanduser(path))
@@ -61,6 +82,11 @@ def get_names_from_wiki():
 
     return names
 
+def log(text):
+    fp = open(LOG_FILENAME, 'a')
+    fp.write('%s,%s\n' % (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), text))
+    fp.close()
+
 class LaserLogWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="LaserLog")
@@ -73,7 +99,6 @@ class LaserLogWindow(Gtk.Window):
         self.grid.set_column_spacing(8)
         self.add(self.grid)
         self.set_default_size(600, 600)
-        self.name = None
 
         # Search Entry for filtering
         self.search_entry = Gtk.SearchEntry()
@@ -101,7 +126,7 @@ class LaserLogWindow(Gtk.Window):
         self.not_in_list = Gtk.Button.new_with_mnemonic("Ik sta niet in de l_ijst")
         self.not_in_list.connect("clicked", self.on_not_in_list)
 
-        self.open_wiki = Gtk.Button.new_with_mnemonic("Meer _informatie op de wiki")
+        self.open_wiki = Gtk.Button.new_with_mnemonic("Meer informatie op de wi_ki")
         self.open_wiki.connect("clicked", self.on_wiki)
 
         self.view_log = Gtk.Button.new_with_mnemonic("Bekijk l_og")
@@ -152,37 +177,22 @@ class LaserLogWindow(Gtk.Window):
         os.spawnvp(os.P_NOWAIT, 'xdg-open', ['xdg-open', LOG_FILENAME])
 
     def on_start_laserweb(self, widget):
-        print("Starting LaserWeb")
-        Gtk.main_quit()
-        fp = open(LOG_FILENAME, 'a')
-        fp.write('%s,%s,%s,%s\n' % (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), self.name, 'LaserWeb', 'start'))
-        fp.close()
-        os.spawnvp(os.P_WAIT, os.path.expanduser('./start-laserweb'), ['start-laserweb'])
-        Gio.DesktopAppInfo.new_from_filename(os.path.expanduser('~/Desktop/LaserWeb.desktop')).launch()
-        fp = open(LOG_FILENAME, 'a')
-        fp.write('%s,%s,%s,%s\n' % (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), self.name, 'LaserWeb', 'done'))
-        fp.close()
-        Gtk.main()
+        run('laserweb')
 
     def on_start_lightburn(self, widget):
-        print("Starting LightBurn")
-        Gtk.main_quit()
-        fp = open(LOG_FILENAME, 'a')
-        fp.write('%s,%s,%s,%s\n' % (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), self.name, 'LightBurn', 'start'))
-        fp.close()
-        os.spawnvp(os.P_WAIT, os.path.expanduser('./start-lightburn'), ['start-lightburn'])
-        fp = open(LOG_FILENAME, 'a')
-        fp.write('%s,%s,%s,%s\n' % (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), self.name, 'LightBurn', 'done'))
-        fp.close()
-        Gtk.main()
+        run('lightburn')
 
     def on_select(self, widget):
+        global name
         sel = widget.get_selected()
         if sel[1] is not None:
-            self.name = sel[0].get_value(sel[1], 0)
+            name = sel[0].get_value(sel[1], 0)
 
 LOG_FILENAME = check_path(LOG_FILENAME)
 CACHE_FILENAME = check_path(CACHE_FILENAME)
+
+name = 'N/A'
+
 win = LaserLogWindow()
 win.connect("destroy", Gtk.main_quit)
 win.show_all()
